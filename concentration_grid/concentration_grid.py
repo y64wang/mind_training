aaf0337eac06d9bb84d607a5fb3b1d24e3c1df9a
Yong Wang <yong.9.wang@nokia.com>
2016-10-21 09:57:56 +0800

    Add concentration grid

---
+++ concentration_grid/concentration_grid.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+import sys, getopt
+import random
+
+def print_grid(num, skip):
+  
+  skip_num = random.randint(0, num*num-1)
+
+  its = range(0, num*num)
+
+  random.shuffle(its)
+
+  print "\n\n".join(map(lambda idx:'\t'.join(map(lambda item: '%02d'%item if (not skip) or (item != skip_num) else "  ", its[idx:idx+num])), range(0, len(its), num)))
+
+
+def main(argv):
+   num = 6
+   skip = False
+   try:
+      opts, args = getopt.getopt(argv,"hn:s")
+   except getopt.GetoptError:
+      print 'concentration-grid.py -n <num> -s'
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print 'concentration-grid.py -n <num> -s'
+         sys.exit()
+      elif opt in ("-n"):
+         num = int(arg)
+      elif opt in ("-s"):
+         skip = True
+
+   print_grid(num, skip) 
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
