38aa19503f0bfce148be57781522d9739cbfcfdf
Yong Wang <yong.9.wang@nokia.com>
2017-11-01 00:07:03 +0800

    LongMenHotel updated

--- codejam/LongMenHotel/LongMenHotel.py
+++ codejam/LongMenHotel/LongMenHotel.py
@@ -3,24 +3,86 @@
 
 import sys, getopt
 
+MAXDIST = 999
+
+def Dijkstra(start, distMap):
+    nodeNum = len(distMap)
+    shortPath = [0 for i in range(nodeNum)]
+    visited = [False for i in range(nodeNum)]
+
+    shortPath[start] = 0
+    visited[start] = True
+
+    for i in range(nodeNum -1):
+        minDist = MAXDIST
+        foundNode = start
+        for node in range(nodeNum):
+            if visited[node] == False and not distMap[start][node] == MAXDIST:
+                if minDist > distMap[start][node]:
+                    minDist = distMap[start][node]
+                    foundNode = node
+
+        shortPath[foundNode] = minDist
+        visited[foundNode] = True
+
+        for node in range(nodeNum):
+            if visited[node] == False and not distMap[foundNode][node] == MAXDIST:
+                sumDist = minDist + distMap[foundNode][node]
+                if distMap[start][node] > sumDist:
+                    distMap[start][node] = sumDist
+
+    return shortPath
+        
+
+    
+
 def CheckRoute(info, longMenMap):
-    result = "YES"
+    result = "NO"
 
-    for i in range(int(info[1])):
-        L_Idx = [ j for j,x in enumerate(longMenMap[i]) if x == 'L' ]
+    L_Pos = Z_Pos = 0
+    
+    for i in range(info[1]):
+        L_Idx = [ j for j,x in enumerate(longMenMap[i]) if x== 'L' ]
         if not L_Idx == []:
-            L_Idx.insert(0, i)
-            break
+            L_Pos = i * info[0] + L_Idx[0]
+            break;
 
-    for i in range(int(info[1])):
-        Z_Idx = [ j for j,x in enumerate(longMenMap[i]) if x == 'Z' ]
+    for i in range(info[1]):
+        Z_Idx = [ j for j,x in enumerate(longMenMap[i]) if x== 'Z' ]
         if not Z_Idx == []:
-            Z_Idx.insert(0, i)
-            break
+            Z_Pos = i * info[0] + Z_Idx[0] 
+            break;
 
-    print L_Idx
-    print Z_Idx
+    if L_Idx == []  or Z_Idx == []:
+        return result
 
+    nodeNum = info[0] * info[1]
+    distMap = [([] * nodeNum) for i in range(nodeNum) ]
+    for n in range(nodeNum):
+        src_x = n / info[0]
+        src_y = n % info[0]
+        src = longMenMap[src_x][src_y]
+        for m in range(nodeNum):
+            dest_x = m / info[0]
+            dest_y = m % info[0]
+            dest = longMenMap[dest_x][dest_y]
+            if src_x == dest_x and src_y == dest_y:
+                distMap[n].append(0)
+            elif (src_x == dest_x and abs(src_y - dest_y) == 1) or  \
+                 (src_y == dest_y and abs(src_x - dest_x) == 1):
+                if src == '*' or dest == '*':
+                    distMap[n].append(MAXDIST)
+                else:
+                    distMap[n].append(1)
+            else:
+                distMap[n].append(MAXDIST)
+
+    shortPath = Dijkstra(Z_Pos, distMap) 
+
+    if shortPath[L_Pos] > info[2]:
+        result = "NO"
+    else:
+        result = "YES"
     return result
 
 def main(argv):
@@ -47,13 +109,14 @@ def main(argv):
             numCases = int(inFileHandle.readline())
             for caseNo in range(numCases):
                 info = inFileHandle.readline().split(' ')
+                info = map(int, info)
                 longMenMap = []
-                for i in range(int(info[1])):
+                for i in range(info[1]):
                     line = inFileHandle.readline().strip('\n')
                     line = line.split(' ')
                     longMenMap.append(line)
-                CheckRoute(info, longMenMap)
-                #outFileHandle.write(result + "\n")
+                res = CheckRoute(info, longMenMap)
+                outFileHandle.write("Case #" + str(caseNo+1) + ": " + res + "\n") 
 
 if __name__ == "__main__":
   main(sys.argv[1:])
