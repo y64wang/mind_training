62b4d000c28dbcbfb0ee8b4d1cc21b7bb31c1410
Yong Wang <yong.9.wang@nokia.com>
2019-12-31 10:53:56 +0800

    update checkuser.sh

--- scripts/checkuser.sh
+++ scripts/checkuser.sh
@@ -3,6 +3,7 @@
 #set -x
 
 KEYWORDS="/mail:/ || /cn:/"
+KEYWORDS="/mail:/"
 
 if [ "$#" -lt 1 ]; then
     echo "$0 userid ..."
@@ -11,7 +12,7 @@ fi
 
 for var in "$@"
 do
-    echo -n "$var: "
-    ldapsearch -H ldaps://ed-p-gl.emea.nsn-net.net -x -b ou=People,o=NSN "uid=$var" | awk "$KEYWORDS"
+    #echo -n "$var: "
+    ldapsearch -H ldaps://ed-p-gl.emea.nsn-net.net -x -b ou=People,o=NSN "uid=$var" | awk "$KEYWORDS" | cut -d " " -f 2
     if [ "$?" -eq 1 ]; then echo; fi
 done
